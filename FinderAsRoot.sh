7f1d10bf985f7d0075bc202ae167bce51d6aad93
vdsau <54153998+vdsau@users.noreply.github.com>
2021-12-07 20:26:37 +0200

    Add files via upload

---
+++ FinderAsRoot.sh
@@ -0,0 +1,11 @@
+# 07.12.2021
+# by @vdsau
+
+sudo /System/Library/CoreServices/Finder.app/Contents/MacOS/Finder > /dev/null 2>&1 & 
+
+sudo osascript -e 'tell application "Terminal" to activate' \
+  -e 'tell application "System Events" to tell process "Terminal" to keystroke "t" using command down' \
+  -e 'tell application "Terminal" to do script "killall Finder" in selected tab of the front window' \
+  -e 'tell application "Terminal" to do script "open ." in selected tab of the front window'
+
+sudo default write com.apple.finder AppleShowAllFiles TRUE
